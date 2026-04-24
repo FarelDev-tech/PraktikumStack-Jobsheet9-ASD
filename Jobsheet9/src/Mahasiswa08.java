383fe8daa607a7b5bd8378a636a38402b339c7d4
fareldev <ffarelscho@gmail.com>
2026-04-24 07:14:58 +0700

    Add Percobaan 1 Stack

---
+++ Jobsheet9/src/Mahasiswa08.java
@@ -0,0 +1,20 @@
+public class Mahasiswa08 {
+    String nama;
+    String nim;
+    String kelas;
+    int nilai;
+
+    public Mahasiswa08(String nama, String nim, String kelas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        
+        nilai = -1;
+    }
+
+    public void tugasDinilai(int nilai) {
+        this.nilai = nilai;
+    }
+
+    
+}
